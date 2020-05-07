51901ed6729c16f92657983cb9c03306a5d76f84
Lineu Pastorelli <lineualberto@gmail.com>
2020-05-07 08:18:25 -0300

    finishing package

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export function useMoney(): number;
+export function useCpf(): number;
+export function useResedentialPhone(): number;
+export function useMobilePhone(): number;
+export function useRg(): number;
