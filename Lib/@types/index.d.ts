aa18e6b49866d00728d1ca50f660b1599d969327
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-11-14 12:02:47 -0300

    added types project

---
+++ Lib/@types/index.d.ts
@@ -0,0 +1,6 @@
+export function useMoney(): string;
+export function useCpf(): string;
+export function useCep(): string;
+export function useResedentialPhone(): string;
+export function useMobilePhone(): string;
+export function useRg(): string;
