2e9d4af14ead9d5b09d5b76830e48edf691a2c07
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-11-14 12:03:39 -0300

    added main file

---
+++ Lib/index.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.useRg = exports.useResedentialPhone = exports.useMobilePhone = exports.useCep = exports.useCpf = exports.useMoney = void 0;
+var useFormat_1 = require("./useFormat");
+exports.useMoney = useFormat_1.useMoney;
+exports.useCpf = useFormat_1.useCpf;
+exports.useCep = useFormat_1.useCep;
+exports.useMobilePhone = useFormat_1.useMobilePhone;
+exports.useResedentialPhone = useFormat_1.useResedentialPhone;
+exports.useRg = useFormat_1.useRg;
