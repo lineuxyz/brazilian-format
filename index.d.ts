747d337133cd5c8a7c1964fd86102bd72a774f09
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-11-14 11:59:32 -0300

    removed files

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-export function useMoney(): number;
-export function useCpf(): number;
-export function useResedentialPhone(): number;
-export function useMobilePhone(): number;
-export function useRg(): number;
