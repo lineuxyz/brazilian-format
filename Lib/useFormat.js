1d2da95000595427a8b7dfd93c82f0bda6f38aa9
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-12-09 19:53:18 -0300

    added new functions

--- Lib/useFormat.js
+++ Lib/useFormat.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
-exports.useRg = exports.useResedentialPhone = exports.useMobilePhone = exports.useCep = exports.useCpf = exports.useMoney = void 0;
+exports.useIsoDateToDateString = exports.useWebsiteRegex = exports.useEmailRegex = exports.usePhoneRegex = exports.useCepRegex = exports.useRg = exports.useResedentialPhone = exports.useMobilePhone = exports.useCep = exports.useCpf = exports.useMoney = void 0;
 var numbResolve_hook_1 = require("./Hooks/numbResolve.hook");
 function useMoney(money) {
     if (!money)
@@ -22,6 +22,8 @@ function useCep(cep) {
     return cepValidate.replace(/(\d{5})(\d{3})/, "\$1-\$2");
 }
 exports.useCep = useCep;
+console.log('12345678');
+console.log(12345678);
 function useResedentialPhone(resedentialPhone) {
     var mobilePhoneValidate = numbResolve_hook_1.numbResolve(resedentialPhone);
     if (mobilePhoneValidate.length <= 10)
@@ -43,3 +45,15 @@ function useRg(rg) {
     return String(rg).replace(/(\d{2})(\d{3})(\d{3})(\d{1})/, "\$1.\$2.\$3-\$4");
 }
 exports.useRg = useRg;
+function useIsoDateToDateString(date) {
+    return date.slice(8, 10) + "/" + date.slice(5, 7) + "/" + date.slice(0, 4);
+}
+exports.useIsoDateToDateString = useIsoDateToDateString;
+var useCepRegex = /\d{5}-?\d{3}/;
+exports.useCepRegex = useCepRegex;
+var usePhoneRegex = /\(\d{2}\) \d{5} \d{4}/;
+exports.usePhoneRegex = usePhoneRegex;
+var useEmailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+exports.useEmailRegex = useEmailRegex;
+var useWebsiteRegex = /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
+exports.useWebsiteRegex = useWebsiteRegex;
